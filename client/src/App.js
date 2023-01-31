fad37a300bbdb960411568c41f651af3ba23bd13
onticinc <onticinc@gmail.com>
2023-01-30 21:48:30 -0700

    added delete functionalitiy

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,28 @@ import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache}
 from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 
--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -2,11 +2,13 @@ import { gql } from '@apollo/client';
 
 const DELETE_CLIENT = gql`
     mutation deleteClient($id: ID!) {
+        deleteClient(id: $id) {
         id
         name
         email
         phone
+        }
     }
 `;
 
-export { DELETE_CLIENT}
+export { DELETE_CLIENT }
