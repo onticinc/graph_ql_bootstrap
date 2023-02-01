075de845fe6d60c45e50be45a38d8c3ad20e05e2
onticinc <onticinc@gmail.com>
2023-02-01 00:07:22 -0700

    got pages working

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import Header from "./components/Header";
-import Clients from "./components/Clients";
-import { ApolloProvider, ApolloClient, InMemoryCache}
-from '@apollo/client';
-import AddClientModal from "./components/addClientModal";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -11,7 +12,7 @@ const cache = new InMemoryCache({
         clients: {
           merge(existing, incoming) {
             return incoming;
-          }, 
+          },
         },
         projects: {
           merge(existing, incoming) {
@@ -22,22 +23,26 @@ const cache = new InMemoryCache({
     },
   },
 });
- 
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
   cache,
 });
 
-
 function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
